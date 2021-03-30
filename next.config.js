0f4258333a60bae0d9c3869cf5bc4c80d766eeca
Danca Prima <dancaprimaa@gmail.com>
2021-03-30 14:05:39 +0700

    add webpack 5 futue

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const {
 const path = require('path');
 
 module.exports = {
+  future: { webpack5: true },
   webpack: (config, options) => {
     const { buildId, dev, isServer, defaultLoaders, webpack } = options;
     const mfConf = {
